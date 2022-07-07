6411a9803bcdd94b5fee26e2bc3cf16aecf87ef3
Daniel <djperrefort@gmail.com>
2022-07-07 12:17:31 -0400

    Add cellrank to requirements

--- app/test_import.py
+++ app/test_import.py
@@ -1,7 +1,7 @@
 """Primary logic for the ``test-import`` application"""
 
 from argparse import ArgumentParser
-
+import sys
 
 class TestImport(ArgumentParser):
     """A demo command line application that verifies package requirements are installed"""
@@ -10,10 +10,11 @@ class TestImport(ArgumentParser):
     def execute(cls):
         """Execute the application"""
 
+        print(f'Using Pytjon {sys.version} - {sys.path}')
 
         import pandas
         print('Pandas was imported successfully!')
         import datasets
         print('Datasets was imported successfully!')
-        import matplotlib
-        print('Matplotlib was imported successfully!')
+        import cellrank
+        print('Cellrank was imported successfully!')
